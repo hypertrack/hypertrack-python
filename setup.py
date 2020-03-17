b6cb750c75da6cf232d2379aa46450ac9e168043
RazumeikoAlex <2330426+RazumeikoAlex@users.noreply.github.com>
2020-03-18 01:13:52 +0200

    Incrementing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hypertrack",
-    version="0.0.1",
+    version="3.0.0",
     author="HyperTrack",
     author_email="help@hypertrack.com",
     description="HyperTrack API wrapper",
