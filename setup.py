dd289bb1c078c9e5a311b2d7290c3896dbc8e3b9
RazumeikoAlex <2330426+RazumeikoAlex@users.noreply.github.com>
2020-03-23 17:56:41 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hypertrack",
-    version="3.0.2",
+    version="3.0.3",
     author="HyperTrack",
     author_email="help@hypertrack.com",
     description="HyperTrack API wrapper",
